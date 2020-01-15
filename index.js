445c8550767a1b2bcaa773d96c72439f3386373c
kristijan-pajtasev <kristijan.pajtasev@hotmail.com>
2020-01-15 01:01:49 +0000

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+const add = (firstNumber, secondNumber) => firstNumber + secondNumber;
+const subtract = (firstNumber, secondNumber) => firstNumber - secondNumber;
+const multiply = (firstNumber, secondNumber) => firstNumber * secondNumber;
+const divide = (firstNumber, secondNumber) => firstNumber / secondNumber;
+
+window.CalculatorHelpers = {
+    add,
+    subtract,
+    multiply,
+    divide
+};
